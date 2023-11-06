f65eeda69a98a74c4d1d099b64450c7960a10265
danebencedavid <danebence@gmail.com>
2023-11-06 15:59:17 +0100

    creating inserts for each table

--- Database/db.py
+++ Database/db.py
@@ -8,3 +8,10 @@ CREATE_TRANSPORTATION = "CREATE TABLE IF NOT EXISTS transportation (id INTEGER P
 CREATE_ENTERTAINMENT = "CREATE TABLE IF NOT EXISTS entertainment (id INTEGER PRIMARY KEY,name TEXT,price INTEGER,date DATE);"
 CREATE_UTILITIES = "CREATE TABLE IF NOT EXISTS utilities (id INTEGER PRIMARY KEY,name TEXT,price INTEGER,date DATE);"
 CREATE_OTHER = "CREATE TABLE IF NOT EXISTS other (id INTEGER PRIMARY KEY,name TEXT,price INTEGER,date DATE);"
+
+INSERT_GROCERIES = "INSERT INTO groceries (name,price,date) VALUES(?, ?, ?);"
+INSERT_TRANSPORTATION = "INSERT INTO transportation (name,price,date) VALUES(?, ?, ?);"
+INSERT_ENTERTAINMENT = "INSERT INTO entertainment (name,price,date) VALUES(?, ?, ?);"
+INSERT_UTILITIES = "INSERT INTO utilities (name,price,date) VALUES(?, ?, ?);"
+INSERT_OTHER = "INSERT INTO other (name,price,date) VALUES(?, ?, ?);"
+
