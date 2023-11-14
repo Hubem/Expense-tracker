89fcdc8a91fecb93e10711a7fa789ad930c9bb7a
Dobos Máté <dobmat2002@gmail.com>
2023-11-14 09:17:06 +0100

    Defined groceries function in main

--- main.py
+++ main.py
@@ -27,7 +27,21 @@ class ExpenseTracker:
         date=val3.get()
         insertion=database(name, price, date)
         return insertion
+    
+    def groceries(self):
+        for widget in self.frame.winfo_children():
+            widget.destroy()
 
+        label_title = Label(self.frame, text='Groceries expenses', font=('Arial', 14, 'bold'))
+        label_title.grid(row=0, column=0, columnspan=2, pady=10)
+
+        label_name = Label(self.frame, text="Name of good").grid(row=1, column=0, sticky=W, pady=2)
+        label_price = Label(self.frame, text="Price").grid(row=2, column=0, sticky=W, pady=2)
+        label_date = Label(self.frame, text="Date of pruchase").grid(row=3, column=0, sticky=W, pady=2)
+
+        entry_name = Entry(self.frame).grid(row=1, column=1, sticky=W, pady=2)
+        entry_price = Entry(self.frame).grid(row=2, column=1, sticky=W, pady=2)
+        entry_date = Entry(self.frame).grid(row=3, column=1, sticky=W, pady=2)
 
 def main():
     root = Tk()
@@ -40,8 +54,8 @@ def main():
     root.config(menu=menubar)
 
     type_menu = Menu(menubar, tearoff=False)
-    type_menu.add_command(label='Groceries expenses')
-    type_menu.add_command(label="Transportation expenses")
+    type_menu.add_command(label='Groceries expenses', command=tracker.groceries)
+    type_menu.add_command(label="Transportation expenses", command=tracker.transportation)
     type_menu.add_command(label="Entertainment expenses")
     type_menu.add_command(label="Utilities expenses")
     type_menu.add_command(label="Other expenses")
