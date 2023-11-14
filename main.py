5f3a8249c786c2983b6f75f496e056ca15196338
Gazdag_Gergő_János <gergogazdag123@gmail.com>
2023-11-14 16:53:21 +0100

    Defined transportation function in main

--- main.py
+++ main.py
@@ -43,6 +43,22 @@ class ExpenseTracker:
         entry_price = Entry(self.frame).grid(row=2, column=1, sticky=W, pady=2)
         entry_date = Entry(self.frame).grid(row=3, column=1, sticky=W, pady=2)
 
+
+    def transportation(self):
+        for widget in self.frame.winfo_children():
+            widget.destroy()
+
+        label_title = Label(self.frame, text='Transportation expenses', font=('Arial', 14, 'bold'))
+        label_title.grid(row=0, column=0, columnspan=2, pady=10)
+
+        label_name = Label(self.frame, text="Name of good").grid(row=1, column=0, sticky=W, pady=2)
+        label_price = Label(self.frame, text="Price").grid(row=2, column=0, sticky=W, pady=2)
+        label_date = Label(self.frame, text="Date of pruchase").grid(row=3, column=0, sticky=W, pady=2)
+
+        entry_name = Entry(self.frame).grid(row=1, column=1, sticky=W, pady=2)
+        entry_price = Entry(self.frame).grid(row=2, column=1, sticky=W, pady=2)
+        entry_date = Entry(self.frame).grid(row=3, column=1, sticky=W, pady=2)
+
 def main():
     root = Tk()
     root.geometry('250x200')
@@ -55,7 +71,7 @@ def main():
 
     type_menu = Menu(menubar, tearoff=False)
     type_menu.add_command(label='Groceries expenses', command=tracker.groceries)
-    type_menu.add_command(label="Transportation expenses")
+    type_menu.add_command(label='Transportation expenses', command=tracker.transportation)
     type_menu.add_command(label="Entertainment expenses")
     type_menu.add_command(label="Utilities expenses")
     type_menu.add_command(label="Other expenses")
